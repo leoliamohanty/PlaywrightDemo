f9bb218dd49f1c25d1d4f701e7e0b53351a7248a
unknown <loelia.mohanty05@gmail.com>
2026-01-18 19:09:15 +0530

    initial push

---
+++ pages/signUpPage.ts
@@ -0,0 +1,43 @@
+import { Page } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+
+
+export default class SignupPage {
+	async load(page: Page) {
+		await page.goto('/signup');
+	}
+
+	private get firstNameInput() {
+		return `[data-testid=first-name]`;
+	}
+
+	private get lastNameInput() {
+		return `[data-testid=last-name]`;
+	}
+
+	private get emailInput() {
+		return `[data-testid=email]`;
+	}
+
+	private get passwordInput() {
+		return `[data-testid=password]`;
+	}
+
+	private get confirmPasswordInput() {
+		return `[data-testid=confirm-password]`;
+	}
+
+	private get submitButton() {
+		return `[data-testid=submit]`;
+	}
+
+	async signup(page: Page) {
+        await page.type(this.firstNameInput, faker.person.firstName());
+        await page.type(this.lastNameInput, faker.person.lastName());
+        await page.type(this.emailInput, faker.internet.email());
+        await page.type(this.passwordInput,'Test@1234');
+        await page.type(this.confirmPasswordInput,'Test@1234');
+        await page.click(this.submitButton);
+
+	}
+}
---
+++ tests/signUp.spec.ts
@@ -0,0 +1,21 @@
+import { faker } from '@faker-js/faker';
+import { test, expect } from '@playwright/test';
+import SignupPage from '../pages/signUpPage';
+
+const signupPage = new SignupPage();
+
+test('has title', async ({ page }) => {
+  await signupPage.load(page);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('QAcart Todo App - Signup page');
+});
+
+test('get started', async ({ page }) => {
+  await signupPage.load(page);
+await signupPage.signup(page);
+
+  // Validate that a success message or specific element is visible
+  await expect(page).toHaveTitle('QAcart Todo App - Todos page');
+
+});
